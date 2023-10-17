904fd016dfffc4281847be72494aef27a92f668d
HuaJiKing402 <huajiking402@outlook.com>
2023-10-17 22:14:19 +0800

    # 修改客户端

--- src/main/java/net/unknownbits/waypoints/Waypoints.java
+++ src/main/java/net/unknownbits/waypoints/Waypoints.java
@@ -1,13 +1,20 @@
 package net.unknownbits.waypoints;
 
+import com.mojang.brigadier.CommandDispatcher;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.Version;
-import net.unknownbits.waypoints.commands.CManager;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.network.message.MessageType;
+import net.minecraft.network.message.SignedMessage;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.unknownbits.waypoints.commands.WaypointsCommand;
 import net.unknownbits.waypoints.entity.Waypoint;
-import net.unknownbits.waypoints.events.EManager;
+import net.unknownbits.waypoints.events.ShareMessageEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,9 +28,17 @@ public class Waypoints implements ModInitializer {
     public static final Version VERSION = FabricLoader.getInstance().getModContainer(MOD_ID).get().getMetadata().getVersion();
     public static List<Waypoint> waypointList = new ArrayList<>();
 
+    public static void EventsRegister(SignedMessage message, ServerPlayerEntity sender, MessageType.Parameters params) {
+        ShareMessageEvents.register(message, sender, params);
+    }
+
+    public static void CommandsRegister(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess, CommandManager.RegistrationEnvironment environment) {
+        WaypointsCommand.register(dispatcher);
+    }
+
     @Override
     public void onInitialize() {
-        CommandRegistrationCallback.EVENT.register(CManager::register);
-        ServerMessageEvents.CHAT_MESSAGE.register(EManager::register);
+        CommandRegistrationCallback.EVENT.register(Waypoints::CommandsRegister);
+        ServerMessageEvents.CHAT_MESSAGE.register(Waypoints::EventsRegister);
     }
 }
--- src/main/java/net/unknownbits/waypoints/entity/Waypoint.java
+++ src/main/java/net/unknownbits/waypoints/entity/Waypoint.java
@@ -1,25 +1,81 @@
 package net.unknownbits.waypoints.entity;
 
 import com.mojang.authlib.GameProfile;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.Vec3i;
 
-public class Waypoint extends Vec3i {
-    private GameProfile gameProfile;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
-    public Waypoint(int x, int y, int z, GameProfile gameProfile) {
-        super(x, y, z);
-        this.gameProfile = gameProfile;
+public class Waypoint {
+    private final UUID uuid;
+    private final GameProfile creator;
+    private final Instant createtimestamp;
+    private final List<GameProfile> authors = new ArrayList<>();
+    private Vec3i position;
+    private Instant lastmodifytimestamp;
+    private Text id;
+    private Text description;
+
+    public Waypoint(GameProfile creator, Vec3i position, Instant cts) {
+        this.id = Text.empty();
+        this.description = Text.empty();
+        this.creator = creator;
+        this.position = position;
+        this.createtimestamp = cts;
+        this.lastmodifytimestamp = cts;
+        this.uuid = UUID.randomUUID();
     }
 
-    public Waypoint(Vec3i pos, GameProfile gameProfile) {
-        this(pos.getX(), pos.getY(), pos.getZ(), gameProfile);
+    public Waypoint(GameProfile creator, int x, int y, int z, Instant cts) {
+        this(creator, new Vec3i(x, y, z), cts);
     }
 
-    public GameProfile getGameProfile() {
-        return gameProfile;
+    public UUID getUuid() {
+        return uuid;
     }
 
-    public void setGameProfile(GameProfile gameProfile) {
-        this.gameProfile = gameProfile;
+    public GameProfile getCreator() {
+        return creator;
+    }
+
+    public List<GameProfile> getAuthors() {
+        return authors;
+    }
+
+    public Vec3i getPosition() {
+        return position;
+    }
+
+    public Instant getCreatetimestamp() {
+        return createtimestamp;
+    }
+
+    public Instant getLastmodifytimestamp() {
+        return lastmodifytimestamp;
+    }
+
+    public Text getId() {
+        return id;
+    }
+
+    public void setId(Text id) {
+        this.id = id;
+    }
+
+    public Text getDescription() {
+        return description;
+    }
+
+    public void setDescription(Text description) {
+        this.description = description;
+    }
+
+    public void modify(GameProfile author, Vec3i position, Instant lmts) {
+        this.authors.add(author);
+        this.position = position;
+        this.lastmodifytimestamp = lmts;
     }
 }
